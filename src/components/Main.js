77ad87e2f84c9a0f078d9ba5402955130e8bb1a0
Marine TEISSIER <marine.teissier@groupedubreuil.com>
2022-10-14 12:20:52 +0200

    Main WIP

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,36 @@
+import styles from "./Main.module.scss";
+
 function Main() {
   return (
-    <main className="flex-fill">
-      <h1>CookChef</h1>
-      <div className="row">
-        <div className="col-12 col-lg-6">Colonne</div>
-        <div className="col-12 col-lg-3">Colonne</div>
-        <div className="col-12 col-lg-3">Colonne</div>
+    <main className={`${styles.mainContent} flex-fill`}>
+      <div className="container">
+        <h1 className="text-center">Découvrez nos nouvelles recettes</h1>
+        <ul className={`${styles.recipeList} row p-3`}>
+          <li className={`${styles.recipeItem} col-12 col-sm-6 col-md-4 col-xl-3`}>
+            <div className={`${styles.recipeItem__container} p-3`}>Recette</div>
+          </li>
+          <li className={`${styles.recipeItem} col-12 col-sm-6 col-md-4 col-xl-3`}>
+            <div className={`${styles.recipeItem__container} p-3`}>Recette</div>
+          </li>
+          <li className={`${styles.recipeItem} col-12 col-sm-6 col-md-4 col-xl-3`}>
+            <div className={`${styles.recipeItem__container} p-3`}>Recette</div>
+          </li>
+          <li className={`${styles.recipeItem} col-12 col-sm-6 col-md-4 col-xl-3`}>
+            <div className={`${styles.recipeItem__container} p-3`}>Recette</div>
+          </li>
+          <li className={`${styles.recipeItem} col-12 col-sm-6 col-md-4 col-xl-3`}>
+            <div className={`${styles.recipeItem__container} p-3`}>Recette</div>
+          </li>
+          <li className={`${styles.recipeItem} col-12 col-sm-6 col-md-4 col-xl-3`}>
+            <div className={`${styles.recipeItem__container} p-3`}>Recette</div>
+          </li>
+          <li className={`${styles.recipeItem} col-12 col-sm-6 col-md-4 col-xl-3`}>
+            <div className={`${styles.recipeItem__container} p-3`}>Recette</div>
+          </li>
+          <li className={`${styles.recipeItem} col-12 col-sm-6 col-md-4 col-xl-3`}>
+            <div className={`${styles.recipeItem__container} p-3`}>Recette</div>
+          </li>
+        </ul>
       </div>
     </main>
   );
