d9345c006ea34f559ef8a8e90ee15aa9a928a000
Marine TEISSIER <marine.teissier@groupedubreuil.com>
2022-11-02 15:56:23 +0100

    Favorite WIP - btn state et list handler

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,36 +1,46 @@
+import { useState } from "react";
 import Recipe from "./Recipe";
 import styles from "./Main.module.scss";
+import { data } from "../data/recipes";
 
 function Main() {
+  const recipeList = data;
+  const [favoriteList, setFavoriteList] = useState([]);
+
+  // Debug
+  console.log(favoriteList);
+
+  const handleFavorite = (id) => {
+    // 1st, check if recipe already exists in favorites list
+    const alreadyFavorite = favoriteList.some((item) => item.id === id);
+
+    if (alreadyFavorite) {
+      // 2nd, if already exists, remove it from the favorites list
+      setFavoriteList(favoriteList.filter((item) => item.id !== id));
+    } else {
+      // 3rd, if doesn't already exists, add it to the favorites list
+      const newFavorite = recipeList.filter((item) => item.id === id);
+      setFavoriteList([...favoriteList, newFavorite[0]]);
+    }
+  };
+
   return (
     <main className={`${styles.mainContent} flex-fill`}>
       <div className="container">
-        <h2 className="text-center mb-5 pb-3">Découvrez nos nouvelles recettes</h2>
+        <h2 className="text-center mb-5 pb-3">
+          Découvrez nos nouvelles recettes
+        </h2>
         <ul className={`${styles.recipeList} row p-3`}>
-          <li className={`col-12 col-sm-6 col-md-4 col-xl-3`}>
-            <Recipe />
-          </li>
-          <li className={`col-12 col-sm-6 col-md-4 col-xl-3`}>
-            <Recipe />
-          </li>
-          <li className={`col-12 col-sm-6 col-md-4 col-xl-3`}>
-            <Recipe />
-          </li>
-          <li className={`col-12 col-sm-6 col-md-4 col-xl-3`}>
-            <Recipe />
-          </li>
-          <li className={`col-12 col-sm-6 col-md-4 col-xl-3`}>
-            <Recipe />
-          </li>
-          <li className={`col-12 col-sm-6 col-md-4 col-xl-3`}>
-            <Recipe />
-          </li>
-          <li className={`col-12 col-sm-6 col-md-4 col-xl-3`}>
-            <Recipe />
-          </li>
-          <li className={`col-12 col-sm-6 col-md-4 col-xl-3`}>
-            <Recipe />
-          </li>
+          {recipeList.map((item) => (
+            <li key={item.id} className="col-12 col-sm-6 col-md-4 col-xl-3">
+              <Recipe
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                favoriteHandler={handleFavorite}
+              />
+            </li>
+          ))}
         </ul>
       </div>
     </main>
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,13 +1,29 @@
+import { useState } from "react";
 import styles from "./Recipe.module.scss";
-import recipeImg from "../assets/images/recipe.webp";
 
-function Recipe() {
+function Recipe({ id, title, image, favoriteHandler }) {
+  // Favorite state of the recipe
+  const [isActive, setIsActive] = useState(false);
+
   return (
     <div className={`${styles.recipeItem__container}`}>
       <div className={`${styles.recipeItem__img}`}>
-        <img src={recipeImg} alt="Oeuf cocotte d'Halloween" />
+        <button
+          className={`${styles.recipeItem__favorite} btn btn--filled ${
+            isActive ? "btn--primaryReverse" : "btn--primary"
+          }`}
+          onClick={() => {
+            setIsActive(!isActive);
+            favoriteHandler(id);
+          }}
+        >
+          <i className="icon fa-solid fa-heart"></i>
+        </button>
+        <img src={image} alt={title} />
       </div>
-	  <h3 className={`${styles.recipeItem__title} text-center p-5 mb-0`}>Oeuf cocotte d'Halloween</h3>
+      <h3 className={`${styles.recipeItem__title} text-center p-5 mb-0`}>
+        {title}
+      </h3>
     </div>
   );
 }
