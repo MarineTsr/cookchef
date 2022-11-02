e9d466db6d619347453b79b43accf373afe2485e
Marine TEISSIER <marine.teissier@groupedubreuil.com>
2022-11-02 16:47:19 +0100

    Update header and add display class

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,25 +6,26 @@ function Header() {
     <header
       className={`${styles.headerMain} d-flex flex-row align-items-center justify-content-between`}
     >
-      <button className="burger">
-        <i className="fa-solid fa-bars"></i>
-      </button>
-
       <h1 className={`${styles.headerMain__logo} mb-0`}>
         <img src={logo} alt="Logo CookChef" />
       </h1>
 
-      <div className="d-flex">
-        <button type="button" className="btn btn--outlined btn--primary">
-          <i className="fa-solid fa-cart-shopping"></i>
-          <span className="ml-2">Mon panier</span>
+      <div className="d-flex ml-4">
+        <button
+          type="button"
+          className="btn btn--outlined btn--primary"
+          title="Ma wishlist"
+        >
+          <i className="icon fa-solid fa-heart"></i>
+          <span className="ml-2 d-none d-sm-inline">Ma wishlist</span>
         </button>
         <button
           type="button"
           className="btn btn--filled btn--primary ml-3 ml-lg-4"
+          title="Connexion"
         >
           <i className="fa-solid fa-user"></i>
-          <span className="ml-2">Connexion</span>
+          <span className="ml-2 d-none d-sm-inline">Connexion</span>
         </button>
       </div>
     </header>
