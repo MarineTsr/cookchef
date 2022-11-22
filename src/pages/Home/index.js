db70c43a2c487ed9b4f1b70c5ad50202d79138ec
Marine TEISSIER <marine.teissier@groupedubreuil.com>
2022-11-22 12:00:21 +0100

    Add pagination and fix filter

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -7,8 +7,11 @@ import styles from "./Home.module.scss";
 
 function Home() {
   const BASE_URL_API = useContext(ApiContext);
+  const RECIPE_LIST_JUMP = 8;
+  const RECIPE_LIST_LENGHT = 40; // Fake value, to prevent fetching results without pagination - to be updated later
+  const [page, setPage] = useState(1);
   const [recipeList, setRecipeList] = useState([]);
-  const [filteredList, setFilteredList] = useState(recipeList);
+  const [filter, setFilter] = useState("");
   const [isLoading, setIsLoading] = useState(true);
 
   // Fetching datas
@@ -17,15 +20,22 @@ function Home() {
 
     const getRecipes = async () => {
       try {
-        setIsLoading(true);
-        const response = await fetch(BASE_URL_API);
+        const response = await fetch(
+          `${BASE_URL_API}?skip=${
+            (page - 1) * RECIPE_LIST_JUMP
+          }&limit=${RECIPE_LIST_JUMP}`
+        );
 
         if (response.ok && !ignoreResponse) {
           const recipes = await response.json();
 
-          // Dyma server returns an array if > 1 todo, otherwise a document
-          setRecipeList(Array.isArray ? recipes : [recipes]);
-          setFilteredList(Array.isArray ? recipes : [recipes]);
+          // Dyma server returns an array if > 1 recipe, otherwise a document
+          // /!\ Use an arrow function in the setter to avoid infinite loop (if setRecipeList has not this func, then recipeList should been added to the dependances) /!\
+          setRecipeList((current) =>
+            Array.isArray(recipes)
+              ? [...current, ...recipes]
+              : [...current, recipes]
+          );
         }
       } catch (e) {
         console.error(e);
@@ -39,7 +49,7 @@ function Home() {
     return () => {
       ignoreResponse = true;
     };
-  }, [BASE_URL_API]);
+  }, [BASE_URL_API, page]);
 
   // Favorites
   const updateRecipe = async (item) => {
@@ -60,9 +70,6 @@ function Home() {
         setRecipeList(
           recipeList.map((item) => (item._id === recipe._id ? recipe : item))
         );
-        setFilteredList(
-          recipeList.map((item) => (item._id === recipe._id ? recipe : item))
-        );
       }
     } catch (e) {
       console.error(e);
@@ -72,14 +79,7 @@ function Home() {
   // Search
   const handleInput = (event) => {
     const filter = event.target.value.trim().toLowerCase();
-
-    if (filter.length >= 3) {
-      setFilteredList(
-        recipeList.filter((item) => item.title.toLowerCase().includes(filter))
-      );
-    } else {
-      setFilteredList(recipeList);
-    }
+    setFilter(filter);
   };
 
   return (
@@ -91,25 +91,49 @@ function Home() {
 
         <SearchBar inputHandler={handleInput} />
 
-        {isLoading ? (
+        {isLoading && !recipeList.lenght ? (
           <Loader classes="mt-5 pt-5" />
         ) : (
-          <ul className={`${styles.recipeList} row p-3 mt-5`}>
-            {filteredList.length === 0 ? (
-              <li className="col-12 text-center">
-                Aucune recette ne correspond à votre recherche
-              </li>
-            ) : (
-              filteredList.map((item) => (
-                <li
-                  key={item._id}
-                  className="col-12 col-sm-6 col-md-4 col-xl-3"
-                >
-                  <RecipeSummary item={item} favoriteHandler={updateRecipe} />
+          <div className={`${styles.recipeList}`}>
+            <ul className="row p-3 mt-5">
+              {!recipeList.filter((item) =>
+                item.title.toLowerCase().includes(filter)
+              ).length ? (
+                <li className="col-12 text-center">
+                  Aucune recette ne correspond à votre recherche
                 </li>
-              ))
-            )}
-          </ul>
+              ) : (
+                recipeList
+                  .filter((item) => item.title.toLowerCase().includes(filter))
+                  .map((item) => (
+                    <li
+                      key={item._id}
+                      className="col-12 col-sm-6 col-md-4 col-xl-3"
+                    >
+                      <RecipeSummary
+                        item={item}
+                        favoriteHandler={updateRecipe}
+                      />
+                    </li>
+                  ))
+              )}
+            </ul>
+
+            {recipeList.filter((item) =>
+              item.title.toLowerCase().includes(filter)
+            ).length &&
+            page * RECIPE_LIST_JUMP <= RECIPE_LIST_LENGHT - RECIPE_LIST_JUMP ? (
+              <div className="d-flex justify-content-center pb-5">
+                <button
+                  type="button"
+                  className="btn btn--filled btn--primary"
+                  onClick={() => setPage(page + 1)}
+                >
+                  Plus de recettes
+                </button>
+              </div>
+            ) : null}
+          </div>
         )}
       </div>
     </main>
