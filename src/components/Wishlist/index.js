e8b6f1b865d1345656d5eb98856030d4990a0849
Marine TEISSIER <marine.teissier@groupedubreuil.com>
2023-01-20 10:27:32 +0100

    Testing TransitionGroup on Wishlist items

--- src/components/Wishlist/index.js
+++ src/components/Wishlist/index.js
@@ -1,6 +1,8 @@
 import styles from "./Wishlist.module.scss";
+import { createRef } from "react";
 import { useSetRecoilState, useRecoilValue } from "recoil";
 import { showWishlistState, wishlistRecipesSelector } from "state";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 function Wishlist({ favoriteHandler }) {
   const setShowWishlist = useSetRecoilState(showWishlistState);
@@ -19,26 +21,38 @@ function Wishlist({ favoriteHandler }) {
 
         {wishlistRecipes.length > 0 ? (
           <ul className="mt-5">
-            {wishlistRecipes.map((item) => (
-              <li
-                key={item._id}
-                className="d-flex align-items-center justify-content-between my-2"
-              >
-                {item.title}
-                <button
-                  className="btn btn--filled btn--primary"
-                  title="Retirer"
-                  onClick={(event) =>
-                    favoriteHandler(
-                      { ...item, isFavorite: !item.isFavorite },
-                      event
-                    )
-                  }
-                >
-                  <i className="fa-solid fa-trash"></i>
-                </button>
-              </li>
-            ))}
+            <TransitionGroup>
+              {wishlistRecipes.map((item) => {
+                const currentRef = createRef(null);
+                return (
+                  <CSSTransition
+                    key={item._id}
+                    nodeRef={currentRef}
+                    timeout={350}
+                    classNames={styles}
+                  >
+                    <li
+                      ref={currentRef}
+                      className="d-flex align-items-center justify-content-between my-2"
+                    >
+                      {item.title}
+                      <button
+                        className="btn btn--filled btn--primary"
+                        title="Retirer"
+                        onClick={(event) =>
+                          favoriteHandler(
+                            { ...item, isFavorite: !item.isFavorite },
+                            event
+                          )
+                        }
+                      >
+                        <i className="fa-solid fa-trash"></i>
+                      </button>
+                    </li>
+                  </CSSTransition>
+                );
+              })}
+            </TransitionGroup>
           </ul>
         ) : (
           <p>Aucun favoris pour le moment</p>
